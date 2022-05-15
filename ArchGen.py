3525d5c4646f751b69640b27996f3b2d86ba59e7
Surya M <smanikha@purdue.edu>
2022-05-15 19:10:51 -0400

    AHHHHHHH

--- ArchGen.py
+++ ArchGen.py
@@ -17,6 +17,7 @@ class ArchGen:
         print('Arch Linux is a great way to learn the inner workings of a\nLinux distro. Therefore, it is highly recommended that users\nattempt to install Arch Linux manually at least once using\nthe excellent wiki.\n')
         print('All to say, this script is not a replacement for that learning\nexperience. The target audience are those who understand the\ninstallation process, but don\'t have the energy to redo it.\n')
         self.awaitConf()
+        self.setNetwork()
 
     def setNetwork(self):
         self.clear()
@@ -24,10 +25,10 @@ class ArchGen:
         print('a network connection is required to proceed. If you have already\nconnected via ethernet, you may skip. Otherwise, setup wifi now.')
         if input('Use WiFi? (y/n): ') == 'y':
             os.system('iwctl device list')
-            adapter = input('Enter Desired Device Name (ex. wlan0): '')
+            adapter = input('Enter Desired Device Name (ex. wlan0): ')
 
             confAdapter = 'Use Device "' + adapter + '? (y/n): '
             if input(confAdapter) == 'n':
-                adapter = input('Enter Desired Device Name (ex. wlan0): '')
+                adapter = input('Enter Desired Device Name (ex. wlan0): ')
 
 installer = ArchGen()
