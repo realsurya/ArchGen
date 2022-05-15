df01c4a6af7100bc7cd5b8c051080d408abd43da
Surya M <smanikha@purdue.edu>
2022-05-15 19:02:12 -0400

    Testing wifi

--- ArchGen.py
+++ ArchGen.py
@@ -18,4 +18,16 @@ class ArchGen:
         print('All to say, this script is not a replacement for that learning\nexperience. The target audience are those who understand the\ninstallation process, but don\'t have the energy to redo it.\n')
         self.awaitConf()
 
+    def setNetwork(self):
+        self.clear()
+        print('-----------------------Network Connection-----------------------\n')
+        print('a network connection is required to proceed. If you have already\nconnected via ethernet, you may skip. Otherwise, setup wifi now.')
+       if input('Use WiFi? (y/n): ') == 'y':
+           os.system('iwctl device list')
+           adapter = input('Enter Desired Device Name (ex. wlan0): '')
+
+           confAdapter = 'Use Device "' + adapter + '? (y/n): '
+           if input(confAdapter) == 'n':
+               adapter = input('Enter Desired Device Name (ex. wlan0): '')
+
 installer = ArchGen()
